c75fcf25d4e53beab62205f9b9a95bfd8a7a47eb
Ibragim.Ast <ibragim.ast@gmail.com>
2022-09-02 00:00:42 +0300

    deleted the excess

--- Task057/Program.cs
+++ Task057/Program.cs
@@ -1,30 +1,56 @@
 ﻿// Задача 57. Задайте массив. Выведите количество одинаковых элементов массива
-// Одномерный массив
+// Двумерный массив
 
-int[] CreateArray(int length, int min, int max)
+//---Создание матрицы с рандомным заполнением---//
+int[,] CreateMatrix(int row, int column, int min, int max)
 {
-    int[] array = new int[length];
-    for (int i = 0; i < length; i++)
+    int[,] matrixArr = new int[row, column];
+    for (int i = 0; i < matrixArr.GetLength(0); i++)
     {
-        array[i] = new Random().Next(min, max);
+        for (int j = 0; j < matrixArr.GetLength(1); j++)
+        {
+            matrixArr[i, j] = new Random().Next(min, max);
+        }
     }
-    Array.Sort(array);
-    return array;
+    return matrixArr;
 }
 
-void PrintArray(int[] array)
+//---Печать матрицы---//
+void PrintMatrix(int[,] arr)
 {
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        System.Console.Write($"{array[i]} ");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j]} ");
+        }
+        System.Console.WriteLine();
     }
     System.Console.WriteLine();
 }
 
+//---Конвертирование двумерного массива в одномерный---//
+int[] ConverterMatrixToArray(int[,] matrixArray)
+{
+    int length = matrixArray.GetLength(0) * matrixArray.GetLength(1);
+    int[] array = new int[length];
+    int z = 0;
+    for (int i = 0; i < matrixArray.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrixArray.GetLength(0); j++)
+        {
+            array[z] = matrixArray[j, i];
+            z++;
+        }  
+    }
+    return array;
+}
+
+//---Подсчет количества повторений чисел в массиве---///
 void FindCountNumber(int[] array)
 {
     int count = 0;
-   
+    Array.Sort(array); //Сортировка массива
     for (int i = 0; i < array.Length; i++)
     {
         count = 0;
@@ -40,7 +66,9 @@ void FindCountNumber(int[] array)
     }  
 }
 
-int[] array = CreateArray(10, 0, 10);
-PrintArray(array);
+int[,] matrixArr = CreateMatrix(3, 10, 0, 10);
+PrintMatrix(matrixArr);
+int[] array = ConverterMatrixToArray(matrixArr);
 FindCountNumber(array);
+
 
